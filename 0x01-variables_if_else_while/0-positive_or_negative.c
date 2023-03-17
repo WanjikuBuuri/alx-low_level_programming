56e1f28524f57df825d08fa64f1ed6a89c2355eb
Wanjiku Buuri <wanjikubuuri@gmail.com>
2023-03-17 12:49:39 -0700

    fixed Betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/**
+ * main - a function that checks if a function is negative or positive or zero
+ * Return: zero always a success
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
